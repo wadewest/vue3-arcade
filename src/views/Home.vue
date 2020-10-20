ba12e815005aed95437fa28723fda4af21057d33
Wade West <wwest81@gmail.com>
2020-10-19 21:09:29 -0500

    Added more info to home page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
+    <h1>A Place to Play Around with Vuejs 3</h1>
     <router-link :to="{name:'Disc Destroyer'}">Disc Destroyer</router-link>
+    <div>
+      Source available at <a href="https://github.com/wadewest/vue3-arcade">https://github.com/wadewest/vue3-arcade</a>
+    </div>
   </div>
 </template>
 
