8a6eefd3db95018ea4b748b87e17b24b4b72e5a6
Wade West <wwest81@gmail.com>
2020-10-24 22:22:48 -0500

    Some cleanup of the Home page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="home">
     <h1>A Place to Play Around with Vuejs 3</h1>
-    <router-link :to="{name:'Disc Destroyer'}">Disc Destroyer</router-link>
+    <h2>Games</h2>
+    <ul>
+      <li><router-link :to="{name:'Disc Destroyer'}">Disc Destroyer</router-link></li>
+    </ul>
     <div>
-      Source available at <a href="https://github.com/wadewest/vue3-arcade">https://github.com/wadewest/vue3-arcade</a>
+      <small>Source available at <a href="https://github.com/wadewest/vue3-arcade">https://github.com/wadewest/vue3-arcade</a></small>
     </div>
   </div>
 </template>
